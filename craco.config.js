4e54fef07ab8d4edf8b3d19f44121ca4589e7176
Eranga Manchanayake <erangakm@gmail.com>
2021-08-22 13:57:36 +1000

    add background tasks

--- craco.config.js
+++ craco.config.js
@@ -6,10 +6,11 @@ module.exports = {
               entry: {
                   main: [env === 'development' && require.resolve('react-dev-utils/webpackHotDevClient'),paths.appIndexJs].filter(Boolean),
                   content: './src/chromeServices/DOMEvaluator.ts',
+                  background: './src/backgroundServices/background.ts',
               },
               output: {
                   ...webpackConfig.output,
-                  filename: 'static/js/[name].js',
+                  filename: '[name].js',
               },
               optimization: {
                   ...webpackConfig.optimization,
